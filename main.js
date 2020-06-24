5909534fcff94afc2bea1c124947817e2e14570e
Joe Langston <joelangston81@gmail.com>
2020-06-23 19:58:53 -0500

    finished

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
-let coin = {
-    state: 0,
-    flip: function() {
-        /* 1. Randomly set your coin object's "state" property to be either 
-           0 or 1: use "this.state" to access the "state" property on this object.*/
-    },
-    toString: function() {
-        /* 2. Return the string "Heads" or "Tails", depending on whether
-           "this.state" is 0 or 1. */
-    },
-    toHTML: function() {
-        let image = document.createElement('img');
-        /* 3. Set the properties of this image element to show either face-up
-           or face-down, depending on whether this.state is 0 or 1.*/
-        return image;
+  
+
+
+    let coin = {
+        state: 0,
+        flip: function () {
+        let coinflip = Math.floor(Math.random() * 2)
+            this.state = coinflip
+            return this.state
+           
+        },
+        toString: function () {
+            
+            if (this.state === 0) {
+                return 'Heads' + (', ')
+            } else {
+                return 'Tails' + (', ')
+            }
+        },
+        toHTML: function () {
+            let image = document.createElement('img');
+             if (this.state === 0) {
+                image.src ='./images/us-quarter-front.jpg'
+               
+            } else {
+
+                image.src = './images/us-quarter-back.jpg'
+            }
+            return image;
+        }
+
     }
-}
+
+    function display20Flips() {
+        let results = []
+      
+        for (let index = 1; index <= 20; index += 1) {
+            coin.flip()
+            document.write(coin.toString())
+        }
+    }
+    display20Flips()
+    
+    function display20Images() {
+        let results = []
+        
+        for (let index = 1; index <= 20; index += 1) {
+            coin.flip()
+            document.querySelector('body').appendChild(coin.toHTML())
+            
+            
+        }
+    }
+display20Images()
+
+
